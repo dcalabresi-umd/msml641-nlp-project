6a5c694c2b85313da38a56584e9fdcba17063e1f
Damian Calabresi <dcalabre@umd.edu>
2025-12-16 17:39:08 -0500

    Cleaning up code

--- convert-to-pdf.sh
+++
@@ -1,4 +0,0 @@
-pandoc README.md -o report.pdf --pdf-engine=pdflatex -V geometry:margin=1in -V fontsize=11pt --toc --toc-depth=2
-
-pandoc README.md -o report.pdf --pdf-engine=pdflatex -V geometry:margin=1in -V fontsize=10pt --toc --toc-depth=2 -V documentclass=article -V classoption=twocolumn
-
--- report/compile-paper.sh
+++ report/compile-paper.sh
@@ -1,28 +1,13 @@
 #!/bin/bash
 
-# Compile the ACL paper
-# This script runs pdflatex, bibtex, and pdflatex twice more to properly generate citations
-
-echo "Cleaning old files..."
 rm -f paper.aux paper.bbl paper.blg paper.log paper.out
 
-echo "First pdflatex pass..."
 pdflatex -interaction=nonstopmode paper.tex > /dev/null
 
-echo "Running bibtex..."
 bibtex paper 2>&1 | grep -v "Illegal"
 
-echo "Second pdflatex pass..."
 pdflatex -interaction=nonstopmode paper.tex > /dev/null
 
-echo "Third pdflatex pass..."
 pdflatex -interaction=nonstopmode paper.tex
 
-echo ""
-echo "Done! Check paper.pdf"
 ls -lh paper.pdf
-
-# Clean up auxiliary files (optional)
-# Uncomment the following lines if you want to remove auxiliary files
-# echo "Cleaning auxiliary files..."
-# rm -f paper.aux paper.bbl paper.blg paper.log paper.out
